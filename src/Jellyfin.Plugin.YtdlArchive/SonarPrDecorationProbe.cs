24b8b8a50ec7d43ffe5b2267e6631ddbd44aa9a2
Stephen Hoos <shoos@talk.hoos.org>
2026-05-22 11:03:07 -0700

    Test SonarQube PR annotations

---
+++ src/Jellyfin.Plugin.YtdlArchive/SonarPrDecorationProbe.cs
@@ -0,0 +1,15 @@
+namespace Jellyfin.Plugin.YtdlArchive;
+
+internal static class SonarPrDecorationProbe
+{
+    internal static string[] BuildProbeValues()
+    {
+        return
+        [
+            "sonar-pr-decoration-probe",
+            "sonar-pr-decoration-probe",
+            "sonar-pr-decoration-probe",
+            "sonar-pr-decoration-probe"
+        ];
+    }
+}
